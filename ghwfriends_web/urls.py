8728a16b674fc1add87ee15cc48aebf00648389d
cosmic-elevator <rose_suzy@naver.com>
2023-12-12 10:35:33 +0900

    modify: 일부 설정 변경

--- ghwfriends_web/urls.py
+++ ghwfriends_web/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+#from . import views
 
 urlpatterns = [
+    path('', include('mainpage.urls')),
     path('admin/', admin.site.urls),
+    path('freeboard/', include('freeboard.urls'))
+
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from freeboard.models import Freeboard_Post
 
 # Create your views here.
+
+
+def index(request):
+    freeboard_posts = Freeboard_Post.objects.all()
+    return render(
+        request,
+        'mainpage/index.html',
+        {
+            'freeboard_posts' : freeboard_posts
+        }
+    )
