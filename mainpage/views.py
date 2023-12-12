ded4e9a76bb4f5cf8ea52f0603f95c9040f78e30
cosmic-elevator <rose_suzy@naver.com>
2023-12-12 11:37:10 +0900

    add: 메인화면에 신청곡 게시판 출력

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from freeboard.models import Freeboard_Post
+from requestboard.models import SongRequest_Post
 
 # Create your views here.
 
 
 def index(request):
     freeboard_posts = Freeboard_Post.objects.all()
+    requestboard_posts = SongRequest_Post.objects.all()
     return render(
         request,
         'mainpage/index.html',
         {
-            'freeboard_posts' : freeboard_posts
+            'freeboard_posts' : freeboard_posts,
+            'requestboard_posts' : requestboard_posts
         }
     )
