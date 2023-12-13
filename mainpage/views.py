51694ee262f286ad60aed92f6d23cd3449537559
cosmic_elevator <rose_suzy@naver.com>
2023-12-13 19:06:29 +0900

    add: footer 문구 추가

--- mainpage/views.py
+++ mainpage/views.py
@@ -6,13 +6,15 @@ from requestboard.models import SongRequest_Post
 
 
 def index(request):
-    freeboard_posts = Freeboard_Post.objects.all()
-    requestboard_posts = SongRequest_Post.objects.all()
+    freeboard_all_posts = Freeboard_Post.objects.all()
+    requestboard_all_posts = SongRequest_Post.objects.all()
+    freeboard_recent_five_posts = freeboard_all_posts[freeboard_all_posts.count()-5:] if freeboard_all_posts.count() >= 5 else freeboard_all_posts
+    requestboard_recent_five_posts = requestboard_all_posts[requestboard_all_posts.count()-5:] if requestboard_all_posts.count() >= 5 else requestboard_all_posts
     return render(
         request,
         'mainpage/index.html',
         {
-            'freeboard_posts' : freeboard_posts,
-            'requestboard_posts' : requestboard_posts
+            'freeboard_recent_five_posts' : freeboard_recent_five_posts,
+            'requestboard_recent_five_posts' : requestboard_recent_five_posts
         }
     )
