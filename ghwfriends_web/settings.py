f672a3137623774fde4fb28aa56da03176b4b6cc
cosmic-elevator <rose_suzy@naver.com>
2023-12-19 13:11:52 +0900

    add: 그냥 완성함...

--- ghwfriends_web/settings.py
+++ ghwfriends_web/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'freeboard',
     'requestboard',
     'mainpage',
+    'crispy_bootstrap4',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
